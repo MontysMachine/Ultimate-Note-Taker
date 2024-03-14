f508ca961dfd63fb3561b00610ba8913657ec688
Monty Montgomery <monty@montys-mbp.attlocal.net>
2024-03-14 16:43:53 -0500

    completely forgot to add the starter css also it took me way to long to realize why nothing was rendering even though the note was saved to the db and that is because i completely forgot to write the get to retrieve all the data

--- server.js
+++ server.js
@@ -3,21 +3,44 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
+const notesDB = require('./db/db.json')
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-app.get('/api/notes', (req, res) =>{
-    
+app.get('/api/notes', (req,res)=>{
+    res.json(notesDB);
 })
 
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    
+    if (!title || !text) {
+        return res.status(400).json({ error: 'Title and text are required for a new note' });
+    }
 
+    const newNote = {
+        title,
+        text
+    };
 
+    notesDB.push(newNote);
 
+    fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notesDB), (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        console.log('New note has been stored and saved!');
+        res.json(notesDB);
+    });
+});
 
 
 app.listen(PORT, () =>{
