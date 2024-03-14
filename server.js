65558c02414c62dcf322c759f978c86bf90a6dc2
Monty Montgomery <monty@montys-mbp.attlocal.net>
2024-03-14 14:45:22 -0500

    wrote a function to read and parse data from the db file currently making sure it works

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
@@ -15,8 +16,16 @@ app.get('/api/notes', (req, res) =>{
 });
 
 app.get('/api/notes', (req, res) =>{
-    fs.readFile(path.join)
-})
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) =>{
+        if(err){
+            console.log(err);
+            return;
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
 
 
 
